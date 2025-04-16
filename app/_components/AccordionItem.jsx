d1509d9b6133f5f1445c8d62f849d417d84f10fd
Mahmoud Abbas <ma7m0ud.abbas@gmail.com>
2025-04-16 14:52:37 +0200

    edit on topics

--- app/_components/AccordionItem.jsx
+++ app/_components/AccordionItem.jsx
@@ -8,18 +8,18 @@ const AccordionItem = ({ title, children }) => {
   return (
     <div className="w-full border border-gray-300 rounded py-3 my-8 text-sm">
       <div
-        className="flex-between cursor-pointer md:cursor-auto py-2 px-4"
+        className="flex-between cursor-pointer py-2 px-4"
         onClick={() => setIsOpen(!isOpen)}
       >
         <span className="font-medium ">{title}</span>
-        <span className={`md:hidden`}>{isOpen ? <FaMinus /> : <FaPlus />}</span>
+        <span>{isOpen ? <FaMinus /> : <FaPlus />}</span>
       </div>
 
       <div
         className={`transition-all duration-300 ease-in-out ${
           isOpen
             ? "max-h-screen opacity-100"
-            : "max-h-0 opacity-0 overflow-hidden md:max-h-screen md:overflow-auto md:opacity-100"
+            : "max-h-0 opacity-0 overflow-hidden"
         }`}
       >
         <div className="py-2">{children}</div>
